e65622b7c9d75126173c84103325442e7a338652
Koen <koenhabets1@gmail.com>
2023-01-18 21:47:13 +0100

    ...

--- app.py
+++ app.py
@@ -12,13 +12,10 @@ influxClient = InfluxDBClient(url=os.environ.get('influx_url'),
 def on_connect(client, userdata, flags, rc):
     print("Mqtt connected with result code " + str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
     client.subscribe("sensors/trilsensor")
     client.subscribe("Temperatuur")
 
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     message = str(msg.payload.decode("utf-8"))
     if msg.topic == 'Temperatuur':
